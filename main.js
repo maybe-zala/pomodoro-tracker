c71ba32ba2f3b8ba89194082e221360a1fa0beb1
Naizala Farr <naizalafarr@Naizalas-MacBook-Pro.local>
2026-08-28 09:12:22 -0500

    starting utilities box and finished timer etc etc etc

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ function addTask(){
         li.classList.add("list-item"); 
         listContainer.appendChild(li);
         let span = document.createElement("span");
-        span.innerHTML = '<svg xmlns="http://w3.org" width="30" height="30" viewBox="0 0 12 12"><path fill="#ffffff" d="M2.22 2.22a.749.749 0 0 1 1.06 0L6 4.939L8.72 2.22a.749.749 0 1 1 1.06 1.06L7.061 6L9.78 8.72a.749.749 0 1 1-1.06 1.06L6 7.061L3.28 9.78a.749.749 0 1 1-1.06-1.06L4.939 6L2.22 3.28a.749.749 0 0 1 0-1.06Z"/></svg>';
+        span.innerHTML = '<svg x mlns="http://w3.org" width="30" height="30" viewBox="0 0 12 12"><path fill="#ffffff" d="M2.22 2.22a.749.749 0 0 1 1.06 0L6 4.939L8.72 2.22a.749.749 0 1 1 1.06 1.06L7.061 6L9.78 8.72a.749.749 0 1 1-1.06 1.06L6 7.061L3.28 9.78a.749.749 0 1 1-1.06-1.06L4.939 6L2.22 3.28a.749.749 0 0 1 0-1.06Z"/></svg>';
         li.appendChild(span);
     }
     inputBox.value = "";
@@ -43,111 +43,72 @@ showTask();
 
 // pomodoro timer down
 
-//javascriptpro_
-let allModBtn = document.querySelectorAll('.container .mode-btns-box button');
-let focusBtn = document.querySelector('.container .mode-btns-box .focus-btn');
-let shortBreakBtn = document.querySelector('.container .mode-btns-box .short-break-btn');
-let longBreakBtn = document.querySelector('.container .mode-btns-box .long-break-btn');
-let startBtn = document.querySelector('.container .btns .start-btn');
-let pauseBtn = document.querySelector('.container .btns .pause-btn');
-let resetBtn = document.querySelector('.container .btns .reset-btn');
-let time = document.querySelector('.container .timer');
+const timer_type_60 = "60";
+const timer_type_30 = "30";
+const timer_type_5 = "5";
+const sixtyMinsInSeconds =  3600;
+const thirtyMinsInSeconds =  1800;
+const fiveMinsInSeconds =  300;
 
-let setIn;
-let count = 59;
-let paused = true;
-let minCount = 24;
-let active = "focus";
+let pomodoroType = timer_type_60;
+let timerValue = thirtyMinsInSeconds; 
+let allTypes = document.querySelectorAll(".pomodoro .buttons");
+let timer = document.querySelector(".countdown")
+let startButton = document.querySelector(".pomodoro .start")
+let stopButton = document.querySelector(".pomodoro .stop")
+let restartButton = document.querySelector(".pomodoro .restart")
+let progressInterval;
 
-time.innerHTML = `${minCount + 1}:00`;
-
-let appendZero =(value)=>{
-  value = value < 10 ? "0" + value : value;   
-  return value;
-}
-
-resetBtn.addEventListener('click',(resetTime =()=>{
-  pauseTimer();
-  switch(active){
-  case "long":
-     minCount = 14;
-     break;
-  case "short":
-     minCount = 4;
-     break;
-  default:
-     minCount = 24;
-     break;
+let resetTimer = () => {
+  clearInterval(progressInterval);
+  startButton.style.display = "block";
+  stopButton.style.display = "none";
+  if(pomodoroType === "60"){
+    timerValue = sixtyMinsInSeconds;
+  } else if(pomodoroType === "30"){
+    timerValue = thirtyMinsInSeconds;
+  } else{
+    timerValue = fiveMinsInSeconds
   }
-  count = 59;
-  time.innerHTML = `${minCount + 1}:00`;
-})
-)
-
-let removeBtnFocus =()=>{
- allModBtn.forEach((button)=>{
-   button.classList.remove('btn-focus');
- })     
-}
-
-focusBtn.addEventListener('click',()=>{
-  active = "focus";    
- removeBtnFocus();
- focusBtn.classList.add('btn-focus');
- pauseTimer();
- count = 59;
- minCount = 24;
- time.innerHTML = `${minCount + 1}:00`;
-})
-
-shortBreakBtn.addEventListener('click', () => {
-  active = "short";  
-  removeBtnFocus();
-  shortBreakBtn.classList.add('btn-focus');
-  pauseTimer();
-  count = 59;
-  minCount = 4;
-  time.innerHTML = `${minCount + 1}:00`;
-})
-
-longBreakBtn.addEventListener('click', () => {
-  active = "long";  
-  removeBtnFocus();
-  longBreakBtn.classList.add('btn-focus');
-  pauseTimer();
-  count = 59;
-  minCount = 14;
-  time.innerHTML = `${minCount + 1}:00`;
-})
-
-pauseBtn.addEventListener('click',(pauseTimer =()=>{
-   paused = true;    
-   clearInterval(setIn);
-   resetBtn.style.display = 'none';
-   pauseBtn.style.display = 'none';
-   startBtn.style.display = 'block';
-}) 
-);
-
-startBtn.addEventListener('click',()=>{
-  resetBtn.style.display = 'block';
-  pauseBtn.style.display = 'block';
-  startBtn.style.display = 'none';  
+  timerProgress();
   
-  if(paused){
-   paused = false;
-   time.innerHTML = `${appendZero(minCount)}:${appendZero(count)}`;
-   setIn = setInterval(()=>{
-    count--;
-    time.innerHTML = `${appendZero(minCount)}:${appendZero(count)}`;
-    if(count == 0){
-     if(minCount != 0){
-        minCount--;    
-        count = 60;
-     }else{
-        clearInterval(setIn)
-     }   
-    }
-   },1000)
+};
+
+let formatedNumberInMinutes = (number) => {
+    let minutes = Math.trunc(number / 60).toString().padStart(2, '0');
+    let seconds = Math.trunc(number % 60).toString().padStart(2, '0');
+
+    return`${minutes}:${seconds}`;
+}
+
+let timerProgress =()=> {
+    timer.innerHTML = `${formatedNumberInMinutes(timerValue)}`;
+}
+
+let getType = (elem, type) => {
+  for (let x of allTypes) {
+    x.classList.remove("active");
   }
-})
+  elem.classList.add("active");
+  pomodoroType = type;
+  resetTimer();
+}
+
+let startTimer = () => {
+    progressInterval = setInterval(() => {
+        timerValue--;
+        timerProgress();
+    },1000);
+    startButton.style.display = "none";
+    stopButton.style.display = "block";
+}
+
+let stopTimer = () => {
+    clearInterval(progressInterval);
+    startButton.style.display = "block";
+    stopButton.style.display = "none";
+}
+
+restartButton.addEventListener("click", resetTimer);
+startButton.addEventListener("click", startTimer);
+stopButton.addEventListener("click", stopTimer);
